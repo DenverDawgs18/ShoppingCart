2d95cd3b6ccf40ebbe94055713c903afb8fad500
DenverDawgs18 <pautomas55@gmail.com>
2024-04-02 15:24:43 -0400

    Wrote snapshot tests

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,10 +1,37 @@
 import { describe, it, expect } from "vitest";
 import App from '../App.jsx';
-import {render, screen} from '@testing-library/react'
+import Cart from '../Cart.jsx';
+import Card from '../Card.jsx'
+import {render} from '@testing-library/react'
+import { BrowserRouter } from "react-router-dom";
 
 describe('App component', () => {
-    it('renders', () => {
-        const {wrapper} = render(<App />);
+    it('renders against snapshot', () => {
+        const {wrapper} = render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+        );
+        expect(wrapper).toMatchSnapshot()
+    })
+})
+describe('Cart component', () => {
+    it('renders against snapshot', () => {
+        const {wrapper} = render(
+        <BrowserRouter>
+            <Cart />
+        </BrowserRouter>
+        );
+        expect(wrapper).toMatchSnapshot()
+    })
+})
+describe('Card component', () => {
+    it('renders against snapshot', () => {
+        const {wrapper} = render(
+        <BrowserRouter>
+            <Card />
+        </BrowserRouter>
+        );
         expect(wrapper).toMatchSnapshot()
     })
 })
